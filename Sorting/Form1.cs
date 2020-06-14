fe89b6fe5cd0705f87fed6e39c16dde625d00ee9
Andrew Langford <43278589+AndrewLangford6@users.noreply.github.com>
2020-06-14 12:53:56 -0400

    fhf

--- Sorting/Form1.cs
+++ Sorting/Form1.cs
@@ -10,70 +10,126 @@ using System.Windows.Forms;
 
 namespace Sorting
 {
-    public partial class Form1 : Form
+     public partial class Form1 : Form
     {
-        int[] originalArray = { 3, 6, 1, 8, 5, 2 };
-        int[] sortedArray = new int[6];
+
+        int loopCounter;
+        int comparisonCounter;
+        int shiftCounter;
+        int[] selectionSorted = new int[6];
+        int[] bubbleSorted = new int[6];
+        int[] insertionSorted = new int[6];
+
+        Random rando = new Random();
+        int numberONE, numberTWO, numberTHREE, numberFOUR, numberFIVE, numberSIX;
+
+
 
         public Form1()
         {
             InitializeComponent();
 
-            // The 0 parameter indicates what index value in the sorted array
-            // to start adding the sorted values to. 
-            originalArray.CopyTo(sortedArray, 0);
+            numberONE = rando.Next(1, 7);
+            numberTWO = rando.Next(1, 7);
+            numberTHREE = rando.Next(1, 7);
+            numberFOUR = rando.Next(1, 7);
+            numberFIVE = rando.Next(1, 7);
+            numberSIX = rando.Next(1, 7);
 
-            //uncomment whichever sort method you want to test
-            selection(sortedArray);
-            //bubble(sortedArray);
-            //insertion(sortedArray);
+            int[] originalArray = { numberONE, numberTWO, numberTHREE, numberFOUR, numberFIVE, numberSIX };
 
-            originalOutput.Text = sortedOutput.Text = "";
+            originalArray.CopyTo(selectionSorted, 0);
+            originalArray.CopyTo(bubbleSorted, 0);
+            originalArray.CopyTo(insertionSorted, 0);
+
+            selection(selectionSorted);
+            loopCounter = 0;
+            comparisonCounter = 0;
+            shiftCounter = 0;
+
+            bubble(bubbleSorted);
+            loopCounter = 0;
+            comparisonCounter = 0;
+            shiftCounter = 0;
+
+            insertion(insertionSorted);
+
+            originalOutput.Text = selectionSortedOutput.Text = "";
 
             foreach (int i in originalArray)
             {
                 originalOutput.Text += i + "\n";
             }
 
-            foreach (int i in sortedArray)
+            foreach (int i in selectionSorted)
             {
-                sortedOutput.Text += i + "\n";
+                selectionSortedOutput.Text += i + "\n";
+            }
+            foreach (int i in bubbleSorted)
+            {
+                bubbleSortedOutput.Text += i + "\n";
+            }
+            foreach (int i in insertionSorted)
+            {
+                insertionSortedOutput.Text += i + "\n";
             }
         }
-
         public void selection(int[] tempArray)
         {
             int temp;
 
+            //starting stopwatch
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
             for (int i = 0; i < tempArray.Length; i++)
             {
+                loopCounter++;
+
                 for (int j = i + 1; j < tempArray.Length; j++)
                 {
+                    comparisonCounter++;
                     if (tempArray[j] < tempArray[i])
                     {
+                        shiftCounter++;
                         temp = tempArray[i];
                         tempArray[i] = tempArray[j];
                         tempArray[j] = temp;
                     }
                 }
             }
-        }
+            //ending stopwatch
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = Convert.ToString(ts.TotalMilliseconds);
 
+            selectionShiftOutputLabel.Text = Convert.ToString(shiftCounter);
+            selectionLoopOutputLabel.Text = Convert.ToString(loopCounter);
+            selectionComparisonOutputLabel.Text = Convert.ToString(comparisonCounter);
+            selectionTimerOutput.Text = elapsedTime;
+        }
 
         public void bubble(int[] tempArray)
         {
-            int bottom = tempArray.Length-1;
+            int bottom = tempArray.Length - 1;
             int temp;
             Boolean sw = true;
+
+            //starting stopwatch
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
 
             while (sw == true)
             {
                 sw = false;
+                loopCounter++;
 
                 for (int j = 0; j < bottom; j++)
                 {
+                    comparisonCounter++;
                     if (tempArray[j] > tempArray[j + 1])
                     {
+                        shiftCounter++;
                         sw = true;
                         temp = tempArray[j];
                         tempArray[j] = tempArray[j + 1];
@@ -82,27 +138,61 @@ namespace Sorting
                 }
                 bottom--;
             }
+            //ending stopwatch
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = Convert.ToString(ts.TotalMilliseconds);
+
+            bubbleShiftOutputLabel.Text = Convert.ToString(shiftCounter);
+            bubbleLoopOutputLabel.Text = Convert.ToString(loopCounter);
+            bubbleComparisonOutputLabel.Text = Convert.ToString(comparisonCounter);
+            bubbleTimerOutput.Text = elapsedTime;
         }
-
-
 
         public void insertion(int[] tempArray)
         {
             int temp, j;
 
+            //starting stopwatch
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
             for (int n = 0; n < tempArray.Length; n++)
             {
+                loopCounter++;
+
                 temp = tempArray[n];
                 j = n - 1;
-                
-                    while (j >= 0 && tempArray[j] > temp)
-                    {
-                        tempArray[j + 1] = tempArray[j];
-                        j--;
-                    }
-                
+                comparisonCounter++;
+
+                while (j >= 0 && tempArray[j] > temp)
+                {
+                    shiftCounter++;
+
+                    tempArray[j + 1] = tempArray[j];
+                    j--;
+                }
+
                 tempArray[j + 1] = temp;
             }
+            //ending stopwatch
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = Convert.ToString(ts.TotalMilliseconds);
+
+            insertionShiftOutputLabel.Text = Convert.ToString(shiftCounter);
+            insertionLoopOutputLabel.Text = Convert.ToString(loopCounter);
+            insertionComparisonOutputLabel.Text = Convert.ToString(comparisonCounter);
+            insertionTimerOutput.Text = elapsedTime;
+        }
+
+        private void lineLabel_Paint(object sender, PaintEventArgs e)
+        {
+            Font font = new Font("Times New Roman", 18);
+            Brush brush = new SolidBrush(Color.Black);
+            e.Graphics.TranslateTransform(30, 20);
+            e.Graphics.RotateTransform(90);
+            e.Graphics.DrawString("______________________________", font, brush, 0, 0);
         }
     }
 }
